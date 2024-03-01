b6f885cc70029837bb627e7f0c3cf704e0761451
Rens van der Velden <I491926@fhict.nl>
2024-03-01 10:53:33 +0100

    Added Java spring boot

---
+++ src/main/java/com/album/albumrater/AlbumraterApplication.java
@@ -0,0 +1,13 @@
+package com.album.albumrater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumraterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumraterApplication.class, args);
+	}
+
+}
